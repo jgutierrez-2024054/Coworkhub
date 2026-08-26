30383b1e0da7a504f9a2ceef287538a9e03b5e65
jgutierrez-2024054 <jgutierrez-2024054@kinal.edu.gt>
2026-08-26 12:20:56 -0600

    Agregar ruta de prueba en router para debug

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const authenticate = require('../middlewares/auth');
 const authController = require('../controllers/authController');
 
+// Ruta de prueba simple en el router
+router.get('/test-router', (req, res) => res.json({ message: 'Router test works' }));
+
 router.use('/auth', require('./authRoutes'));
 router.use('/plans', require('./planRoutes'));
 router.use('/spaces', require('./spaceRoutes'));
